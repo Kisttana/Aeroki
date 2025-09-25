001bb0766a50ad9f80c00ff4859f49bb94ef836a
Tanathip Nualsuwan <tanathip_it@hotmail.com>
2025-09-25 14:25:23 +0700

    Implement lexer for tokenizing input lines

--- Aeroki/Aeroki.c
+++ Aeroki/Aeroki.c
@@ -38,4 +38,54 @@ typedef enum {
     TOK_ID, TOK_NUM,
     TOK_ASSIGN, TOK_PLUS, TOK_MINUS, TOK_MUL, TOK_DIV,
     TOK_EOF
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char text[64];
+} Token;
+
+Token tokens[128];
+int tok_count, tok_pos;
+
+void lex_line(const char *line) {
+    tok_count = 0; tok_pos = 0;
+    const char *p = line;
+
+    while (*p) {
+        if (isspace(*p)) { p++; continue; }
+
+        if (strncmp(p, "ให้", 6) == 0) {
+            tokens[tok_count++] = (Token){TOK_GIVE, "ให้"}; p += 6;
+        } else if (strncmp(p, "หา", 6) == 0) {
+            tokens[tok_count++] = (Token){TOK_FIND, "หา"}; p += 6;
+        } else if (*p == '=') {
+            tokens[tok_count++] = (Token){TOK_ASSIGN, "="}; p++;
+        } else if (*p == '+') {
+            tokens[tok_count++] = (Token){TOK_PLUS, "+"}; p++;
+        } else if (*p == '-') {
+            tokens[tok_count++] = (Token){TOK_MINUS, "-"}; p++;
+        } else if (*p == '*') {
+            tokens[tok_count++] = (Token){TOK_MUL, "*"}; p++;
+        } else if (*p == '/') {
+            tokens[tok_count++] = (Token){TOK_DIV, "/"}; p++;
+        } else if (isdigit(*p)) {
+            char buf[64]; int len = 0;
+            while (isdigit(*p)) buf[len++] = *p++;
+            buf[len] = '\0';
+            tokens[tok_count++] = (Token){TOK_NUM, ""};
+            strcpy(tokens[tok_count-1].text, buf);
+        } else {
+            char buf[64]; int len = 0;
+            while (*p && !isspace(*p) && *p!='=' && *p!='+' && *p!='-' && *p!='*' && *p!='/')
+                buf[len++] = *p++;
+            buf[len] = '\0';
+            tokens[tok_count++] = (Token){TOK_ID, ""};
+            strcpy(tokens[tok_count-1].text, buf);
+        }
+    }
+    tokens[tok_count++] = (Token){TOK_EOF, ""};
 }
+
+Token *peek() { return &tokens[tok_pos]; }
+Token *next() { return &tokens[tok_pos++]; }
