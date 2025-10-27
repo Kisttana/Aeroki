08b2321870083ad1795bb0360cf27799e9c7dd37
Tanathip Nualsuwan <tanathip_it@hotmail.com>
2025-10-27 16:41:32 +0700

    Refactor token initialization in lexer

--- Parser/lexer.cpp
+++ Parser/lexer.cpp
@@ -1,50 +1,61 @@
 #include "Aeroki.h"
 
+typedef enum {
+    TOK_GIVE, TOK_FIND, TOK_INPUT, TOK_RESULT,
+    TOK_ID, TOK_NUM,
+    TOK_ASSIGN, TOK_PLUS, TOK_MINUS, TOK_MUL, TOK_DIV,
+    TOK_EOF
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char text[64];
+} Token;
+
 Token tokens[128];
-int tok_count = 0;
-int tok_pos = 0;
+int tok_count, tok_pos;
 
 void lex_line(const char *line) {
     tok_count = 0; tok_pos = 0;
     const char *p = line;
 
     while (*p) {
-        if (isspace((unsigned char)*p)) { p++; continue; }
+        if (isspace(*p)) { p++; continue; }
 
         if (strncmp(p, "ให้", strlen("ให้")) == 0) {
-            tokens[tok_count++] = {TOK_GIVE, "ให้"}; p += strlen("ให้");
+            tokens[tok_count++] = (Token){TOK_GIVE, "ให้"}; p += strlen("ให้");
         } else if (strncmp(p, "หา", strlen("หา")) == 0) {
-            tokens[tok_count++] = {TOK_FIND, "หา"}; p += strlen("หา");
+            tokens[tok_count++] = (Token){TOK_FIND, "หา"}; p += strlen("หา");
         } else if (strncmp(p, "รับค่า", strlen("รับค่า")) == 0) {
-            tokens[tok_count++] = {TOK_INPUT, "รับค่า"}; p += strlen("รับค่า");
+            tokens[tok_count++] = (Token){TOK_INPUT, "รับค่า"}; p += strlen("รับค่า");
         } else if (strncmp(p, "ผล", strlen("ผล")) == 0) {
-            tokens[tok_count++] = {TOK_RESULT, "ผล"}; p += strlen("ผล");
+            tokens[tok_count++] = (Token){TOK_RESULT, "ผล"}; p += strlen("ผล");
         } else if (*p == '=') {
-            tokens[tok_count++] = {TOK_ASSIGN, "="}; p++;
+            tokens[tok_count++] = (Token){TOK_ASSIGN, "="}; p++;
         } else if (*p == '+') {
-            tokens[tok_count++] = {TOK_PLUS, "+"}; p++;
+            tokens[tok_count++] = (Token){TOK_PLUS, "+"}; p++;
         } else if (*p == '-') {
-            tokens[tok_count++] = {TOK_MINUS, "-"}; p++;
+            tokens[tok_count++] = (Token){TOK_MINUS, "-"}; p++;
         } else if (*p == '*') {
-            tokens[tok_count++] = {TOK_MUL, "*"}; p++;
+            tokens[tok_count++] = (Token){TOK_MUL, "*"}; p++;
         } else if (*p == '/') {
-            tokens[tok_count++] = {TOK_DIV, "/"}; p++;
-        } else if (isdigit((unsigned char)*p)) {
+            tokens[tok_count++] = (Token){TOK_DIV, "/"}; p++;
+        } else if (isdigit(*p)) {
             char buf[64]; int len = 0;
-            while (isdigit((unsigned char)*p)) buf[len++] = *p++;
+            while (isdigit(*p)) buf[len++] = *p++;
             buf[len] = '\0';
-            tokens[tok_count++] = {TOK_NUM, ""};
+            tokens[tok_count++] = (Token){TOK_NUM, ""};
             strcpy(tokens[tok_count-1].text, buf);
         } else {
             char buf[64]; int len = 0;
-            while (*p && !isspace((unsigned char)*p) && *p!='=' && *p!='+' && *p!='-' && *p!='*' && *p!='/')
+            while (*p && !isspace(*p) && *p!='=' && *p!='+' && *p!='-' && *p!='*' && *p!='/')
                 buf[len++] = *p++;
             buf[len] = '\0';
-            tokens[tok_count++] = {TOK_ID, ""};
+            tokens[tok_count++] = (Token){TOK_ID, ""};
             strcpy(tokens[tok_count-1].text, buf);
         }
     }
-    tokens[tok_count++] = {TOK_EOF, ""};
+    tokens[tok_count++] = (Token){TOK_EOF, ""};
 }
 
 Token *peek() { return &tokens[tok_pos]; }
